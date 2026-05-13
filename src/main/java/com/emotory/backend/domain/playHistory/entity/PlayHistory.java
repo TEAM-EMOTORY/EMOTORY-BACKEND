884d3099f2cba2b37b78082dee575362135e2d6c
jeonghoon11 <jih361@naver.com>
2026-05-13 15:19:03 +0900

    feat: PlayHistory 엔티티 구현

---
+++ src/main/java/com/emotory/backend/domain/playHistory/entity/PlayHistory.java
@@ -0,0 +1,36 @@
+package com.emotory.backend.domain.playHistory.entity;
+
+import com.emotory.backend.domain.choice.entity.Choice;
+import com.emotory.backend.domain.playSession.entity.PlaySession;
+import com.emotory.backend.domain.storyNode.entity.StoryNode;
+import com.emotory.backend.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class PlayHistory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "play_history_id", nullable = false)
+    private Long playHistoryId;
+
+    @Column(name = "step_number", nullable = false)
+    private Integer stepNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "play_session_id", nullable = false)
+    private PlaySession playSession;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "node_id", nullable = false)
+    private StoryNode storyNode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "choice_id", nullable = false)
+    private Choice choice;
+}
