415358c52922ff30b68ccc68ea49383b94c08291
jae-2024 <dbswotkd2019@naver.com>
2026-05-13 10:41:32 +0900

    [Feature] S3 이미지 업로드 및 CloudFront 조회 구조 구현 (#17)
    * feature: S3의존성 추가 + 공통설정 (#16)
    
    * feature: S3 이미지 업로드 다운 및 CloudFront 조회 기능 추가 (#16)
    
    * feature: S3 관련 예외 처리 추가 (#16)

--- src/main/java/com/emotory/backend/global/exception/ErrorCode.java
+++ src/main/java/com/emotory/backend/global/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
     // 400 error
     INVALID_INPUT(400, "잘못된 입력 값입니다."),
     NO_FEED_UPDATE_CONTENT_EXCEPTION(400, "수정할 내용이 없습니다."),
+    UNSUPPORTED_IMAGE_TYPE(400, "지원하지 않는 이미지 타입입니다."),
+    UNSUPPORTED_FILE_FORMAT(400, "지원하지 않는 파일 형식입니다."),
 
     // 401 error
     UNAUTHORIZED(401, "인증이 필요합니다."),
@@ -20,9 +22,11 @@ public enum ErrorCode {
 
     // 404 error
     NOT_FOUND(404, "존재하지 않는 리소스입니다."),
+    S3_FILE_NOT_FOUND(404, "S3 파일을 찾을 수 없습니다."),
 
     // 500 error
-    INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다."),
+    S3_DOWNLOAD_FAILED(500, "S3 파일 다운로드에 실패했습니다.");
 
     private final int status;
     private final String message;
