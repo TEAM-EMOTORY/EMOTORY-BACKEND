8d9bd9f71822913849705d783dc55ea52a44e24f
jae <dbswotkd2019@naver.com>
2026-05-16 09:14:07 +0900

    feat: 이미지 재생성 방지 로직 구현 (#35)

---
+++ src/main/java/com/emotory/backend/domain/storyNode/repository/StoryNodeRepository.java
@@ -0,0 +1,7 @@
+package com.emotory.backend.domain.storyNode.repository;
+
+import com.emotory.backend.domain.storyNode.entity.StoryNode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoryNodeRepository extends JpaRepository <StoryNode, Long> {
+}
---
+++ src/main/java/com/emotory/backend/domain/storyNode/service/StoryNodeService.java
@@ -0,0 +1,4 @@
+package com.emotory.backend.domain.storyNode.service;
+
+public class StoryNodeService {
+}
