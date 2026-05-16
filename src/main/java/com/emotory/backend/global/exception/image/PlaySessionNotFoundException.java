db7120aaa603b180a65e9b0106c397e4dbb0b2a9
jae <dbswotkd2019@naver.com>
2026-05-16 09:19:54 +0900

    feat: OpenAI 예외 처리 및 WebClient 설정 개선 (#35)

---
+++ src/main/java/com/emotory/backend/global/exception/image/PlaySessionNotFoundException.java
@@ -0,0 +1,8 @@
+package com.emotory.backend.global.exception.image;
+
+import com.emotory.backend.global.exception.CustomException;
+import com.emotory.backend.global.exception.ErrorCode;
+
+public class PlaySessionNotFoundException extends CustomException {
+    public PlaySessionNotFoundException() { super(ErrorCode.PLAY_SESSION_NOT_FOUND); }
+}
---
+++ src/main/java/com/emotory/backend/global/exception/image/StoryNodeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.emotory.backend.global.exception.image;
+
+import com.emotory.backend.global.exception.CustomException;
+import com.emotory.backend.global.exception.ErrorCode;
+
+public class StoryNodeNotFoundException extends CustomException {
+    public StoryNodeNotFoundException() { super(ErrorCode.STORY_NODE_NOT_FOUND); }
+}
