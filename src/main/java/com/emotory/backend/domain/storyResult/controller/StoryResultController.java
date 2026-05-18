f7dc2308459a8fe863275354c5936d4cfca5f856
jeonghoon11 <jih361@naver.com>
2026-05-19 04:51:56 +0900

    feat: StoryResult Controller 구현

---
+++ src/main/java/com/emotory/backend/domain/storyResult/controller/StoryResultController.java
@@ -0,0 +1,35 @@
+package com.emotory.backend.domain.storyResult.controller;
+
+import com.emotory.backend.domain.storyResult.dto.request.StoryResultCreateRequest;
+import com.emotory.backend.domain.storyResult.dto.response.StoryResultResponse;
+import com.emotory.backend.domain.storyResult.service.StoryResultService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StoryResultController {
+
+    private final StoryResultService storyResultService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/play-sessions/{playSessionId}/results")
+    public void createStoryResult(
+            @PathVariable Long playSessionId,
+            @Valid @RequestBody StoryResultCr현eateRequest request
+    ) {
+        storyResultService.createStoryResult(
+                playSessionId,
+                request
+        );
+    }
+
+    @GetMapping("/api/story-results/{playSessionId}")
+    public StoryResultResponse getStoryResult(
+            @PathVariable Long playSessionId
+    ) {
+        return storyResultService.getStoryResult(playSessionId);
+    }
+}
