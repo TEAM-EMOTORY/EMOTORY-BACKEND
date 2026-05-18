8362b07e0505c0ffeed8cc7e833fc485f8221b5d
jeonghoon11 <jih361@naver.com>
2026-05-19 02:50:09 +0900

    feat: StoryNode service 구현

---
+++ src/main/java/com/emotory/backend/domain/storyNode/exception/StoryNodeNotFoundException.java
@@ -0,0 +1,11 @@
+package com.emotory.backend.domain.storyNode.exception;
+
+import com.emotory.backend.global.exception.CustomException;
+import com.emotory.backend.global.exception.ErrorCode;
+
+public class StoryNodeNotFoundException extends CustomException {
+
+    public StoryNodeNotFoundException() {
+        super(ErrorCode.STORY_NODE_NOT_FOUND);
+    }
+}
