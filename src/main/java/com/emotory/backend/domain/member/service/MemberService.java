c5bbb2dd849d43de77a46d84833ffe618077ca02
jeonghoon11 <jih361@naver.com>
2026-05-19 01:01:27 +0900

    feat: 멤버 생성 Service 구현

--- src/main/java/com/emotory/backend/domain/member/service/MemberService.java
+++ src/main/java/com/emotory/backend/domain/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.emotory.backend.domain.member.service;
 
+import com.emotory.backend.domain.member.dto.request.MemberCreateRequest;
 import com.emotory.backend.domain.member.dto.response.MemberNameResponse;
 import com.emotory.backend.domain.member.entity.Member;
 import com.emotory.backend.domain.member.exception.MemberNotFoundException;
@@ -14,6 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    @Transactional
+    public void createMember(MemberCreateRequest request) {
+        Member member = Member.create(request.name(), request.faceImageUrl());
+        memberRepository.save(member);
+    }
 
     public MemberNameResponse getMemberName(Long memberId) {
         Member member = memberRepository.findById(memberId)
