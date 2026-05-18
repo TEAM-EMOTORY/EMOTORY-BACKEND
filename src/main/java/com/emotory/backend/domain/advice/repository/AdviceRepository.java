1be8fd93264173940e063c2aedf7900d582abf67
jeonghoon11 <jih361@naver.com>
2026-05-19 04:52:31 +0900

    feat: AdviceRepository 구현

---
+++ src/main/java/com/emotory/backend/domain/advice/repository/AdviceRepository.java
@@ -0,0 +1,11 @@
+package com.emotory.backend.domain.advice.repository;
+
+import com.emotory.backend.domain.advice.entity.Advice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdviceRepository extends JpaRepository<Advice, Long> {
+
+    Optional<Advice> findFirstByStoryResult_IdOrderByIdDesc(Long storyResultId);
+}
